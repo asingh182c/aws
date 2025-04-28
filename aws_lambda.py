de6b6eb8c86fab0ccad87dcc78f741dca3a71245
Ashish-SinghGY <ashish_singh@goodyear.com>
2025-04-28 19:04:35 +0530

    new code

--- aws_lambda.py
+++ aws_lambda.py
@@ -4,33 +4,38 @@ import os
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
-    bucket_name = 'your-source-bucket-name'
-    output_bucket_name = 'your-output-bucket-name'
-    output_folder = 'output-folder/'
-    output_file = 'object_info.csv'
+    bucket_name = 'smarteamtotc-legacypoc'
+    output_folder = 'index/'  # Specify the folder where the CSV file will be saved
     
-    # List objects in the source bucket
-    response = s3.list_objects_v2(Bucket=bucket_name)
+    # Create a temporary file to store CSV data
+    tmp_file = '/tmp/s3_objects.csv'
     
-    # Prepare CSV file
-    csv_file_path = f'/tmp/{output_file}'
-    with open(csv_file_path, mode='w', newline='') as file:
+    # Open the CSV file for writing
+    with open(tmp_file, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['Name', 'URL', 'Type'])
+        writer.writerow(['Folder Name', 'Object Name', 'Size (Bytes)', 'Object URL'])
         
-        # Iterate over each object in the bucket
-        for obj in response.get('Contents', []):
-            object_name = obj['Key']
-            object_url = f"https://{bucket_name}.s3.amazonaws.com/{object_name}"
-            object_type = obj['Key'].split('.')[-1] if '.' in obj['Key'] else 'Unknown'
-            
-            # Write object info to CSV
-            writer.writerow([object_name, object_url, object_type])
+        # List objects in the bucket
+        response = s3.list_objects_v2(Bucket=bucket_name)
+        
+        if 'Contents' in response:
+            for obj in response['Contents']:
+                key = obj['Key']
+                size = obj['Size']
+                
+                if key.endswith('/'):
+                    folder_name = key
+                else:
+                    folder_name = os.path.dirname(key)
+                    object_name = os.path.basename(key)
+                    object_url = f"https://{bucket_name}.smarteamtotc-legacypoc.s3.us-east-1.amazonaws.com/{key}"
+                    
+                    writer.writerow([folder_name, object_name, size, object_url])
     
-    # Upload CSV file to the output folder in the same bucket
-    s3.upload_file(csv_file_path, output_bucket_name, f"{output_folder}{output_file}")
+    # Upload the CSV file to the specified folder in the S3 bucket
+    s3.upload_file(tmp_file, bucket_name, f'{output_folder}s3_objects.csv')
     
     return {
-        'statusCode': 200,
-        'body': f"CSV file created and uploaded to {output_folder}{output_file} in bucket {output_bucket_name}"
+        'statusCode': 0,
+        'body': 'CSV file created and uploaded successfully!'
     }
